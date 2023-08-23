c7ccd826bbd2991dd67970c382303147af5da98b
sabbanisrujana41 <sabbanisrujana41@gmail.com>
2023-08-23 17:35:16 +0530

    added ML assignments

---
+++ The_Code_Corrector/submit/predict.py
@@ -0,0 +1,41 @@
+import numpy as np
+from numpy import random as rand
+import pickle
+
+# DO NOT CHANGE THE NAME OF THIS METHOD OR ITS INPUT OUTPUT BEHAVIOR
+
+# PLEASE BE CAREFUL THAT ERROR CLASS NUMBERS START FROM 1 AND NOT 0. THUS, THE FIFTY ERROR CLASSES ARE
+# NUMBERED AS 1 2 ... 50 AND NOT THE USUAL 0 1 ... 49. PLEASE ALSO NOTE THAT ERROR CLASSES 33, 36, 38
+# NEVER APPEAR IN THE TRAINING SET NOR WILL THEY EVER APPEAR IN THE SECRET TEST SET (THEY ARE TOO RARE)
+
+# Input Convention
+# X: n x d matrix in csr_matrix format containing d-dim (sparse) bag-of-words features for n test data points
+# k: the number of compiler error class guesses to be returned for each test data point in ranked order
+
+# Output Convention
+# The method must return an n x k numpy nd-array (not numpy matrix or scipy matrix) of classes with the i-th row 
+# containing k error classes which it thinks are most likely to be the correct error class for the i-th test point.
+# Class numbers must be returned in ranked order i.e. the label yPred[i][0] must be the best guess for the error class
+# for the i-th data point followed by yPred[i][1] and so on.
+
+# CAUTION: Make sure that you return (yPred below) an n x k numpy nd-array and not a numpy/scipy/sparse matrix
+# Thus, the returned matrix will always be a dense matrix. The evaluation code may misbehave and give unexpected
+# results if an nd-array is not returned. Please be careful that classes are numbered from 1 to 50 and not 0 to 49.
+
+def findErrorClass( X, k ):
+	# Find out how many data points we have
+	n = X.shape[0]
+
+	with open('model.npz', 'rb') as f:
+		model = pickle.load(f)
+
+	yPred = np.zeros((n, k))
+	for i in range(n):
+		b = model.predict_proba(X[i])
+		b = np.insert(b, 0, -1)
+		b = np.insert(b, 33, 0)
+		b = np.insert(b, 36, 0)
+		b = np.insert(b, 38, 0)
+		x_sorted = np.argsort(b)[-k:]
+		yPred[i,:] = x_sorted[::-1]
+	return yPred
